cebf057f78e5ccdd41f1d9a38f58bd3ba8850739
Adam <bottarn.botvidsson@gmail.com>
2019-04-16 08:27:19 +0200

    Face recognition now works for several faces

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './FaceRecognition.css';
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, boxes }) => {
+
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto' />
-        <div className='bounding-box'
-          style={
-            {
-              top: box.topRow,
-              right: box.rightCol,
-              bottom: box.bottomRow,
-              left: box.leftCol
-            }
-          }
-        />
+        {
+          boxes.length ?
+            boxes.map((box, i) =>
+              <div key={i} className='bounding-box'
+                style={
+                  {
+                    top: box.topRow,
+                    right: box.rightCol,
+                    bottom: box.bottomRow,
+                    left: box.leftCol
+                  }
+                }
+              />)
+            : ''
+        }
       </div>
     </div>
   );
